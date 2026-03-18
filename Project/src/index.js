c48c52cd21da72c3ec90941d57b335369b301cb5
Teesha Ramchandani <teeshar@iitbhilai.ac.in>
2026-03-18 19:56:26 +0530

    Connected to Mongo Atlas

---
+++ Project/src/index.js
@@ -0,0 +1,29 @@
+
+import connectDB from "./db/index.js";
+//require("dotenv").config({path:"./.env"});
+import dotenv from "dotenv";
+dotenv.config({path:"./.env"});
+connectDB();
+
+/*
+import express from "express";
+const app = express();
+
+async function connectDB(){
+    try {
+        const connection = await mongoose.connect(process.env.MONGODB_URI + "/" + DB_NAME);
+        app.on("error",()=>{
+            console.log("Error");
+            throw error;
+            
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+}
+*/
