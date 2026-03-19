706a75dae9042f35027b1a8f9840c0f6ad1de049
Teesha Ramchandani <teeshar@iitbhilai.ac.in>
2026-03-19 16:14:47 +0530

    utils discussion

--- Project/src/index.js
+++ Project/src/index.js
@@ -3,7 +3,20 @@ import connectDB from "./db/index.js";
 //require("dotenv").config({path:"./.env"});
 import dotenv from "dotenv";
 dotenv.config({path:"./.env"});
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(err)=>{
+            console.log("Error");
+            throw error;
+            
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((error)=>{
+    console.error("Error connecting to MongoDB:", error);
+})
 
 /*
 import express from "express";
