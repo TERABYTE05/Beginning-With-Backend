7520eb83e394f6c4e1898eefbbf23d33fab677a0
Teesha Ramchandani <teeshar@iitbhilai.ac.in>
2026-03-21 21:06:18 +0530

    router and controller guide

--- Project/src/app.js
+++ Project/src/app.js
@@ -15,4 +15,13 @@ app.use(express.json({limit : "16kb"}));
 app.use(express.urlencoded({extended : true, limit : "16kb"}));
 app.use(express.static("public"));
 
+//routes import
+import userRoute from "./routes/user.routes.js";
+
+
+//routes declaration
+app.use("/api/v1/users",userRoute);
+
+// http://localhost:8000/users/register
+
 export {app}
--- Project/src/index.js
+++ Project/src/index.js
@@ -1,8 +1,11 @@
 
 import connectDB from "./db/index.js";
+import express from "express";
 //require("dotenv").config({path:"./.env"});
 import dotenv from "dotenv";
 dotenv.config({path:"./.env"});
+const app = express();
+
 connectDB()
 .then(()=>{
     app.on("error",(err)=>{
