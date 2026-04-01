c3af878b5f23555240c1a2a6a0ce827b356b92b6
Teesha Ramchandani <teeshar@iitbhilai.ac.in>
2026-04-01 14:39:21 +0530

    controller function names debugged

--- Project/src/routes/user.routes.js
+++ Project/src/routes/user.routes.js
@@ -17,13 +17,13 @@ router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyJWT,logoutUser);
 
-router.route("/refresh-token").post(verifyJWT,refreshTokenHandler);
+router.route("/refresh-token").post(verifyJWT,refreshAccesToken);
 router.route("/change-password").post(verifyJWT,changeCurrentUserPassword);
 router.route("/current-user").get(verifyJWT,getCurrentUserDetails);
 router.route("/update-account").patch(verifyJWT,updateCurrentUserDetails);
-router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateCurrentUserAvatar);
-router.route("/update-cover").patch(verifyJWT,upload.single("coverImage"),updateCurrentUserCoverImage);
-router.route("/c/:username").get(getUserProfileByUsername);
-router.route("/history").get(verifyJWT,getUserHistory);
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),UpdateUserAvatar);
+router.route("/update-cover").patch(verifyJWT,upload.single("coverImage"),UpdateUserCoverImage);
+router.route("/c/:username").get(getUserChannelProfile);
+router.route("/history").get(verifyJWT,GetWatchHistory);
 //router.route("/login").post(login);
 export default router;
